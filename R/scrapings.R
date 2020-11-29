c1d3859722b25dd159e9aed4a65d65348c631307
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-11-29 14:37:21 -0500

    pdf_list passes initial unit test

---
+++ R/scrapings.R
@@ -0,0 +1,9 @@
+pdf_list <- function(page){
+  xml2::read_html(page) %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href") %>%
+    stringr::str_subset("\\.pdf") %>%
+    stringr::str_subset("^((?!publicinfo).)*$") %>%
+    stringr::str_subset("^((?!diff).)*$") %>%
+    stringr::str_c("https://www.supremecourt.gov", .)
+}
