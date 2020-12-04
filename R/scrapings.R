d9893a0b63ffa43258c508966c5f1760d1570eb0
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-12-04 16:28:04 -0500

    Created helper opauth_h1 which checks for a header that indicates the start of a new opinion and loops over all pages to group assign group numbers to different opinions

--- R/scrapings.R
+++ R/scrapings.R
@@ -12,3 +12,23 @@ pdf_list <- function(page){
 opinion_author <- function(srcdoc){
 
 }
+
+opauth_h1 <- function(srcdoc){
+  srcdoc <- srcdoc %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(is_first = stringr::str_detect(.data$value, "\\r\\nSUPREME COURT OF THE UNITED STATES\\r\\n")) %>%
+    dplyr::mutate(group_no = 1)
+
+  for(i in seq_along(srcdoc$is_first)) {
+    if(i != 1){
+      if (srcdoc$is_first[[i]] == "TRUE"){
+        srcdoc$group_no[[i]] <- srcdoc$group_no[[i - 1]] + 1
+      }
+      else if(srcdoc$is_first[[i]] == "FALSE"){
+        srcdoc$group_no[[i]] <- srcdoc$group_no[[i - 1]]
+      }
+    }
+  }
+  srcdoc %>%
+    dplyr::select(-is_first)
+}
