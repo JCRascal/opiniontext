ba103bfbdfe5a83ea14f850839c20492ad90841d
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-12-21 15:00:25 -0500

    quilt_pdf_several was created which returns a tibble which contains the assembled contents of several source pdfs

--- R/quilting.R
+++ R/quilting.R
@@ -1,6 +1,13 @@
+
 quilt_pdf <- function(srcpdf){
   tmp <- pdftools::pdf_text(srcpdf) %>%
     prep_text()
 
   tibble::tibble("text" = text_clean(tmp), "author" = opinion_author(tmp), "type" = opinion_type(tmp))
+}
+
+
+quilt_pdf_several <- function(srclist){
+  srclist %>%
+    purrr::map_dfr(quilt_pdf)
 }
